7a6c78d88377e03eef41cee7a3b6d55f2eb18f00
Javier Torres Gálvez <jtg00023@red.ujaen.es>
2023-03-14 19:17:36 +0100

    Primer Commit

--- ms-plantilla/callbacks.js
+++ ms-plantilla/callbacks.js
@@ -14,7 +14,7 @@ const faunadb = require('faunadb'),
     q = faunadb.query;
 
 const client = new faunadb.Client({
-    secret: '¿¿¿ CLAVE SECRETA EN FAUNA PARA ESTA BBDD???',
+    secret: 'fnAE-8tCzpACWVpWtKMHGDigKcNBKQuG4bWxc_ql',
 });
 
 const COLLECTION = "¿¿¿ COLECCION ???"
